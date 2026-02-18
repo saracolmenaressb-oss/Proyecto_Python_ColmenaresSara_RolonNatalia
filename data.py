c6feae727779f628de8a2cf81af838eae5d500db
nataliarolonleal29 <nataliarolonleal@gmail.com>
2026-02-17 20:34:49 -0500

    data.py

---
+++ data.py
@@ -0,0 +1,31 @@
+trainers_data = [
+        {"nombre":"Pedro","apellido":"Gómez","horario":"6:00-18:00","rutas":["Java","NodeJS"], "grupo": None},
+        {"nombre":"Cristian","apellido":"Díaz","horario":"10:00-18:00","rutas":["Java","NodeJS"], "grupo": None},
+        {"nombre":"Kevin","apellido":"Pinzón","horario":"6:00-14:00","rutas":["Java"], "grupo": None},
+        {"nombre":"Edwin","apellido":"Mendoza","horario":"6:00-14:00","rutas":["Java","Netcore"], "grupo": None},
+        {"nombre":"Jholver","apellido":"Pinto","horario":"6:00-14:00","rutas":["Java","Netcore"], "grupo": None},
+        {"nombre":"Carlos","apellido":"Rodríguez","horario":"18:00-22:00","rutas":["Netcore"], "grupo": None}
+    ]
+rutas_existentes = [{
+    "NodeJS",
+    "Java",
+    "Netcore"
+}]
+campers_cursando={
+    "id": "",
+    "nombre": "",
+    "apellido": "",
+    "estado": "",
+    "grupo": None,
+    "trainer": None,
+    "ruta": None,
+    "riesgo": None,
+    "estado": "Aprobado"
+}
+nuevo_Camper=[]
+lista_campers = []
+nombres = ["Juan","Maria","Luis","Ana","Carlos","Elena","Diego","Paula","Andrés","Lucía"]
+apellidos = ["Arias","Castro","Vargas","Ríos","Duarte","Peña","López","García","Marín"]
+estados = ["En proceso de ingreso","Inscrito","Aprobado","Cursando","Graduado","Expulsado","Retirado"]
+salones = ["Sputnik","Apolo","Artemis"]
+grupos = []
