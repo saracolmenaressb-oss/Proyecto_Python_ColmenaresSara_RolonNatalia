ea6684f69559e94b4adfb1940fb129404c55b28b
nataliarolonleal29 <nataliarolonleal@gmail.com>
2026-02-20 21:22:49 -0500

    Update Proyecto_Python_ColmenaresSara_RolonNatalia.py

--- Proyecto_Python_ColmenaresSara_RolonNatalia.py
+++ Proyecto_Python_ColmenaresSara_RolonNatalia.py
@@ -1,38 +1,42 @@
 from coordinador import gestionar_riesgo
 from coordinador import asignar_trainer
-from coordinador import matricular_camper(lista_campers, lista_grupos)
+from coordinador import matricular_camper()
 
-from camper import calcular_riesgo(puntos)
-from camper import editar_camper(lista_campers)
-from camper import registrar_nota_inicial(lista_campers)
-from camper import ver_trainer(camper)
-from camper import ver_notas(camper)
-from camper import ver_horario(camper)
-from reportes import menu_reportes(lista_campers, lista_trainers)
-from trainer import registrar_nota(trainer, camper_id, nota)
-from trainer import ver_horario_trainer(nombre_trainer, lista_trainers, lista_grupos)
+from camper import calcular_riesgo()
+from camper import editar_camper()
+from camper import registrar_nota_inicial()
+from camper import ver_trainer()
+from camper import ver_notas()
+from camper import ver_horario()
+from reportes import menu_reportes()
+from trainer import registrar_nota()
+from trainer import ver_horario_trainer()
 import data
 
 import random
 from datetime import datetime, timedelta
-
-camper = []
-lista_campers = []
-lista_trainers = []
-trainer = []
-camper_id = []
-nombre_trainer = []
-lista_grupos = []
-nota = []
-puntos = []
+import Json
+    camper = {
+        "nombre": nombre,
+        "id": id_estudiante,
+        "direccion": direccion,
+        "acudiente": acudiente,
+        "estado": None 
+        "Trainer": None
+    }
+    with open("campers.Json", "w") as f:
+        Json.dump(numeros, f)
+        estudiantes.append(nuevo_estudiante)
 
 def menu_principal():
     print("=====================")
     print("BIENVENIDO A PLATAFORMA CAMPUSLANDS")
     print("=====================")
-    print("1. Camper")
-    print("2. Trainer")
-    print("3. Coordinador")
+    print("--Menu principal--")
+    print("1. Registrarme")
+    print("2. Iniciar secion")
+    print("3. salir")
+    print(':')
 def menu_coordinador(lista_campers, rutas_existentes):
     print("-- COORDINACIÓN ACADÉMICA ---")
     print("1. Registrar nota de prueba de admisión")
@@ -44,102 +48,82 @@ def menu_coordinador(lista_campers, rutas_existentes):
     print("7. Reportes")
     print("8. Volver al menú principal")
 def menu_trainer():
-        print('1. Ver estudiantes')
-        print('2. Registrar notas')
-        print('3. Volver al menu principal')
+    iniciar_sesion(trainers_data)
+    print('1. Ver horario')
+    print('2. Registrar notas')
+    print('3. Volver al menu principal')
+    opcion_trainer=int(input('Elije'))
+    if opcion_trainer== '1':
+        ver_horario_trainer(trainers_data)
+    elif opcion_trainer=='2':
+        registrar_nota(trainer, camper_id, nota)
+    elif opcion_trainer=='3':
+        menu_principal()
+def menu_camper():
+    print('Seleccione una opcion: ')
+    print('1. Ver notas')
+    print('2. Ver Trainer')
+    print('3. Volver al menu prinipal')
+    opcion_camper=int(input(':'))
+    if opcion_camper = '1':
+        ver_notas(camper)
+    elif:
+        ver_trainer(camper)
+    elif:
+        menu_principal()
 
-while True:
+
     menu_principal()
-    opcionUsuario = input("Selecciona tu rol (1-3):")
-    if opcionUsuario == "1":
-        print("-- MENU CAMPER ---")
-        print("Seleccione su estado")
-        print("1. cursando")
-        print("2. en proceso de ingreso")
-        print("3. :")
-        opcionEstado=int(input(":"))
-        if opcionEstado==1:
-            nombre= input("Digite su nombre")
-            usuario=input("Digite su usuario")
-            ID= input("Digite su ID")
-            camper_cursando={
-            "nombre" : nombre,
-            "usuario": usuario,
-            "ID": ID,
-            "Estado": "cursando"
-            }
-            lista_campers.append(camper_cursando)
-            print("1. Ver notas")
-            print("2. Ver trainer asignado")
-            print("3. Ver horario asignado")
-            print("4. Volver al menu principal")
-            opcion_camper=int(input("seleccione una opcion"))
-            if(opcion_camper==1):
-                ver_notas(camper_cursando)
-            elif(opcion_camper==2):
-                ver_trainer(camper_cursando)
-            elif(opcion_camper==3):
-                ver_horario_trainer(camper_cursando.get("Trainer"), trainers_data, grupos)
-        elif opcionEstado==2:
-            print("Ingrese sus datos personales: ")
-            nombre= input("Digite su nombre: ")
-            print({nombre})
-            ID= input("Digite su ID: ")
-            print({ID})
-            Direccion= input("Ingrese su direccion: ")
-            Acudiente= input("¿Cual es su acudiente?: ")
-            print("Seleccione en que horario quiere aplicar: ")
-            print("1. Jornada de la mañana(6:00AM-2:00PM)")
-            print("2. Jornada de la tarde(2:00PM-7:00)")
-            print("3. Jornada de la noche(6:00PM-10:00PM)")
-            print(":")
-            opcionJornada=int(input(":"))
-            camper_proceso_ingreso = {
-                "nombre": nombre,
-                "ID": ID,
-                "direccion": Direccion,
-                "acudiente": Acudiente,
-                "Jornada": opcionJornada
-            }
-            nuevo_Camper.append(camper_proceso_ingreso)
-            print("Se le notificara cuando deba presentar la prueba")
-            menu_principal()
-    elif opcionUsuario == '2':
-        menu_trainer()
-        opcion_trainer=int(input('Elije'))
-        if(opcion_trainer==1):
-           print("Función de registrar nota aún no implementada.")
-        elif(opcion_trainer==2):
-            ver_horario_trainer(nombre_trainer, lista_trainers, lista_grupos)
-        elif(opcion_trainer==3):
-            menu_principal()
-    elif opcionUsuario == "3":
-        menu_coordinador(lista_campers, rutas_existentes)
-        opcion_coordinador = input("Selecciona una opción (1, 2 o 3): ")
-        if opcion_coordinador == "1":
-            registrar_nota_inicial(lista_campers)
-        elif opcion_coordinador == "2":
-            gestionar_riesgo(lista_campers)
-        elif opcion_coordinador == "3":
-            print("-- CREAR NUEVA RUTA DE ENTRENAMIENTO --")
-            nombre_ruta = input("Nombre de la nueva ruta: ")
-            bd_principal = input("Base de datos principal: ")
-            bd_alternativo = input("Base de datos alternativa: ")
-            nueva_ruta = {
-                "Nombre": nombre_ruta,
-                "Módulos": ["Fundamentos de programación", "Programación Web", "Programación formal", "Bases de datos", "Backend"],
-                "BD": [bd_principal, bd_alternativo]
+    opcionUsuario=int(input(':'))
+    if opcionUsuario=='1':
+        print("Rellene la siguiente informacion")
+        validar_nombre()
+        Apellido=input("Digite su apellido: ")
+        validar_id()
+        Direccion= input("Digite su direccion: ")
+        Acudiente= input("Digite su acudiente: ")
+        validar_telefono()
+    
+    elif opcionEstado == '2':
+        print("Seleccione su rol")
+        print("1. camper")
+        print("2. Coordinadora")
+        print("3. Trainer")
+        opcion_rol=int(input(':'))
+        if(opcion_rol==1):
+            validar_nombre()
+            validar_id()       
+            usuario=input("Digite su usuario: ")
+            menu_camper()
+        elif opcion_rol=='2':
+            menu_trainer()
+        elif opcion_rol=='3':
+            menu_coordinador(lista_campers, rutas_existentes)
+            opcion_coordinador = input("Selecciona una opción (1, 2 o 3): ")
+            if opcion_coordinador == "1":
+                registrar_nota_inicial(lista_campers)
+            elif opcion_coordinador == "2":
+                    gestionar_riesgo(lista_campers)
+            elif opcion_coordinador == "3":
+                print("-- CREAR NUEVA RUTA DE ENTRENAMIENTO --")
+                nombre_ruta = input("Nombre de la nueva ruta: ")
+                bd_principal = input("Base de datos principal: ")
+                bd_alternativo = input("Base de datos alternativa: ")
+                nueva_ruta = {
+                    "Nombre": nombre_ruta,
+                    "Módulos": ["Fundamentos de programación", "Programación Web", "Programación formal", "Bases de datos", "Backend"],
+                    "BD": [bd_principal, bd_alternativo]
                 }
-            rutas_existentes.append(nueva_ruta)
-            print(f"Ruta '{nombre_ruta}' creada existosamente en el sistema.")
-        elif opcion_coordinador == "4":
-            matricular_camper(lista_campers, grupos)
-        elif opcion_coordinador == "5":
-            editar_camper(lista_campers)
-        elif opcion_coordinador == "6":
-            menu_reportes(lista_campers)
-        elif opcion_coordinador == "7":
-            print("Volviendo al menú principal...")
-            menu_principal()
-    else:
-        print("Opción no válida. Por favor, seleccione una opción del 1 al 7.")
+                rutas_existentes.append(nueva_ruta)
+                print(f"Ruta '{nombre_ruta}' creada existosamente en el sistema.")
+            elif opcion_coordinador == "4":
+                matricular_camper(lista_campers, grupos)
+            elif opcion_coordinador == "5":
+                editar_camper(lista_campers)
+            elif opcion_coordinador == "6":
+                menu_reportes(lista_campers)
+            elif opcion_coordinador == "7":
+                print("Volviendo al menú principal...")
+                menu_principal()
+            else:
+                print("Opción no válida. Por favor, seleccione una opción del 1 al 7.")
